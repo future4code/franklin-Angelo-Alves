5d4a2d3f45b74440a014b404e83d4806d3600b71
Angelo Alves <angelo.alves@grupoboticario.com.br>
2022-06-20 14:59:38 -0300

    habilitando criação de playlists

--- modulo1/projeto-labefy/labefy/src/App.js
+++ modulo1/projeto-labefy/labefy/src/App.js
@@ -2,61 +2,44 @@
 import './App.css';
 import axios from 'axios'
 import React, { useState } from 'react';
+import styled from 'styled-components'
+import { Playlist } from './components/Playlist';
+import { ListaPlaylist } from './components/ListaPlaylists';
 
+const Li = styled.li`
+list-style: none;
+font-style: italic;
+margin`
 
 function App() {
+  const [etapa, setEtapa] = useState('principal')
 
 
-  function getAll(){
-    axios.get('https://us-central1-labenu-apis.cloudfunctions.net/labefy/playlists',{
-      headers: {
-        Authorization: 'angelo-alves-franklin'
-      }
-    }).then((response) => {
-      console.log(response)
-      // setPlaylists(response.data.result.list)
-    }).catch((error) => {
-      console.log(error)
-    })
-    
-  }
-
-  function createPlaylist(){
-    let body = {
-      "name": input
+  const RenderizaEtapa = () => {
+		switch (etapa) {
+	      case 'playlist': 
+	        return (<Playlist />)
+	      case 'principal': 
+	        return (<ListaPlaylist  etapa={setEtapa} />)
     }
-    axios.post('https://us-central1-labenu-apis.cloudfunctions.net/labefy/playlists',body, {
-      headers: {
-        Authorization: 'angelo-alves-franklin'
-      }
-    }).then((response) => {
-      console.log(response.data)
-    }).catch((error) => {
-      console.log(error)
-    })
-    getAll();
   }
-  
+  function mudaEtapa(){
+    if (etapa === 'principal'){
+      setEtapa('playlist')
+    }
+    else if (etapa === 'playlist'){
+      setEtapa('principal')
+    }
+  }
 
-  function addPlaylist(){
-    console.log(input)
-  }
-  getAll()
-  const [playlists, setPlaylists] = useState([])
-  const [input, setInput] = useState('')
-  const handleInput = (event) =>{
-    setInput(event.target.value)
-  }
-  return (
-    <div className="App">
-      <h1>labefy</h1>
-      <div> 
-        <input value={input} onChange={handleInput} />
-        <button onClick={createPlaylist}>criar playlist</button> 
-      </div>
-      {playlists}
-    </div>
-  );
+   return (
+     <div className="App">
+       <button onClick={mudaEtapa}>mostra</button> 
+       <RenderizaEtapa />
+      
+     </div>
+
+   );
 }
 
 export default App;
---
+++ modulo1/projeto-labefy/labefy/src/components/Playlist.js
@@ -0,0 +1,12 @@
+//import './App.css';
+//import axios from 'axios'
+import React, { useState } from 'react';
+//import styled from 'styled-components'
+
+function Playlist() {
+    return(
+        <h1>oie</h1>
+    )
+}
+
+export { Playlist }
